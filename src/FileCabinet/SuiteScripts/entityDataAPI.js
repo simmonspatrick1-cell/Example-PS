e1cab8a09683162ecd9316b687d165ba9a2099ec
simmonspatrick1-cell <simmonspatrick1@gmail.com>
2025-11-27 04:28:38 -0700

    Add RESTlet API script for customer and item data operations

---
+++ src/FileCabinet/SuiteScripts/entityDataAPI.js
@@ -0,0 +1,399 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType Restlet
+ * @NModuleScope SameAccount
+ */
+define(['N/record', 'N/search', 'N/log', 'N/error', 'N/http'],
+
+function(record, search, log, error, http) {
+    
+    /**
+     * Function called upon sending a GET request to the RESTlet.
+     *
+     * @param {Object} requestParams - Parameters from HTTP request URL; parameters will be passed into function as an Object (for all supported content types)
+     * @returns {string | Object} HTTP response body; return string when request Content-Type is 'text/plain'; return Object when request Content-Type is 'application/json'
+     * @since 2015.2
+     */
+    function doGet(requestParams) {
+        try {
+            var entityType = requestParams.entity;
+            var entityId = requestParams.id;
+            
+            if (!entityType) {
+                return {
+                    success: false,
+                    message: 'Entity type is required. Use "customers" or "items".'
+                };
+            }
+            
+            if (entityType === 'customers') {
+                return handleCustomerGet(entityId);
+            } else if (entityType === 'items') {
+                return handleItemGet(entityId);
+            } else {
+                return {
+                    success: false,
+                    message: 'Unsupported entity type. Use "customers" or "items".'
+                };
+            }
+        } catch (e) {
+            log.error({
+                title: 'Error in GET request',
+                details: e.toString()
+            });
+            return {
+                success: false,
+                message: 'An error occurred: ' + e.toString()
+            };
+        }
+    }
+
+    /**
+     * Function called upon sending a PUT request to the RESTlet.
+     *
+     * @param {string | Object} requestBody - The HTTP request body; request body will be passed into function as a string when request Content-Type is 'text/plain' or as an Object when request Content-Type is 'application/json'
+     * @returns {string | Object} HTTP response body; return string when request Content-Type is 'text/plain'; return Object when request Content-Type is 'application/json'
+     * @since 2015.2
+     */
+    function doPut(requestBody) {
+        try {
+            var data = typeof requestBody === 'string' ? JSON.parse(requestBody) : requestBody;
+            var entityType = data.entity;
+            var entityId = data.id;
+            
+            if (!entityType) {
+                return {
+                    success: false,
+                    message: 'Entity type is required. Use "customers" or "items".'
+                };
+            }
+            
+            if (!entityId) {
+                return {
+                    success: false,
+                    message: 'Entity ID is required for update operations.'
+                };
+            }
+            
+            if (entityType === 'customers') {
+                return handleCustomerPut(entityId, data);
+            } else if (entityType === 'items') {
+                return handleItemPut(entityId, data);
+            } else {
+                return {
+                    success: false,
+                    message: 'Unsupported entity type. Use "customers" or "items".'
+                };
+            }
+        } catch (e) {
+            log.error({
+                title: 'Error in PUT request',
+                details: e.toString()
+            });
+            return {
+                success: false,
+                message: 'An error occurred: ' + e.toString()
+            };
+        }
+    }
+
+    /**
+     * Function called upon sending a POST request to the RESTlet.
+     *
+     * @param {string | Object} requestBody - The HTTP request body; request body will be passed into function as a string when request Content-Type is 'text/plain' or as an Object when request Content-Type is 'application/json'
+     * @returns {string | Object} HTTP response body; return string when request Content-Type is 'text/plain'; return Object when request Content-Type is 'application/json'
+     * @since 2015.2
+     */
+    function doPost(requestBody) {
+        try {
+            var data = typeof requestBody === 'string' ? JSON.parse(requestBody) : requestBody;
+            var entityType = data.entity;
+            
+            if (!entityType) {
+                return {
+                    success: false,
+                    message: 'Entity type is required. Use "customers" or "items".'
+                };
+            }
+            
+            if (entityType === 'customers') {
+                return handleCustomerPost(data);
+            } else if (entityType === 'items') {
+                return handleItemPost(data);
+            } else {
+                return {
+                    success: false,
+                    message: 'Unsupported entity type. Use "customers" or "items".'
+                };
+            }
+        } catch (e) {
+            log.error({
+                title: 'Error in POST request',
+                details: e.toString()
+            });
+            return {
+                success: false,
+                message: 'An error occurred: ' + e.toString()
+            };
+        }
+    }
+
+    /**
+     * Function called upon sending a DELETE request to the RESTlet.
+     *
+     * @param {Object} requestParams - Parameters from HTTP request URL; parameters will be passed into function as an Object (for all supported content types)
+     * @returns {string | Object} HTTP response body; return string when request Content-Type is 'text/plain'; return Object when request Content-Type is 'application/json'
+     * @since 2015.2
+     */
+    function doDelete(requestParams) {
+        try {
+            var entityType = requestParams.entity;
+            var entityId = requestParams.id;
+            
+            if (!entityType) {
+                return {
+                    success: false,
+                    message: 'Entity type is required. Use "customers" or "items".'
+                };
+            }
+            
+            if (!entityId) {
+                return {
+                    success: false,
+                    message: 'Entity ID is required for delete operations.'
+                };
+            }
+            
+            if (entityType === 'customers') {
+                return handleCustomerDelete(entityId);
+            } else if (entityType === 'items') {
+                return handleItemDelete(entityId);
+            } else {
+                return {
+                    success: false,
+                    message: 'Unsupported entity type. Use "customers" or "items".'
+                };
+            }
+        } catch (e) {
+            log.error({
+                title: 'Error in DELETE request',
+                details: e.toString()
+            });
+            return {
+                success: false,
+                message: 'An error occurred: ' + e.toString()
+            };
+        }
+    }
+
+    // Helper functions for Customer operations
+    function handleCustomerGet(customerId) {
+        if (customerId) {
+            var customer = record.load({
+                type: record.Type.CUSTOMER,
+                id: customerId,
+                isDynamic: false
+            });
+            return {
+                success: true,
+                data: {
+                    id: customer.id,
+                    companyName: customer.getValue({ fieldId: 'companyname' }),
+                    email: customer.getValue({ fieldId: 'email' }),
+                    phone: customer.getValue({ fieldId: 'phone' }),
+                    address: customer.getValue({ fieldId: 'defaultaddress' })
+                }
+            };
+        } else {
+            var customerSearch = search.create({
+                type: search.Type.CUSTOMER,
+                columns: ['internalid', 'companyname', 'email', 'phone', 'defaultaddress'],
+                filters: []
+            });
+            var searchResults = customerSearch.run().getRange({ start: 0, end: 100 });
+            var customers = searchResults.map(function(result) {
+                return {
+                    id: result.getValue('internalid'),
+                    companyName: result.getValue('companyname'),
+                    email: result.getValue('email'),
+                    phone: result.getValue('phone'),
+                    address: result.getValue('defaultaddress')
+                };
+            });
+            return {
+                success: true,
+                data: customers
+            };
+        }
+    }
+
+    function handleCustomerPost(data) {
+        var customer = record.create({
+            type: record.Type.CUSTOMER,
+            isDynamic: true
+        });
+        customer.setValue({ fieldId: 'companyname', value: data.companyName });
+        if (data.email) customer.setValue({ fieldId: 'email', value: data.email });
+        if (data.phone) customer.setValue({ fieldId: 'phone', value: data.phone });
+        if (data.address) customer.setValue({ fieldId: 'defaultaddress', value: data.address });
+        
+        var customerId = customer.save();
+        log.audit({
+            title: 'Customer Created',
+            details: 'Customer created with ID: ' + customerId
+        });
+        return {
+            success: true,
+            message: 'Customer created successfully',
+            id: customerId
+        };
+    }
+
+    function handleCustomerPut(customerId, data) {
+        var customer = record.load({
+            type: record.Type.CUSTOMER,
+            id: customerId,
+            isDynamic: true
+        });
+        if (data.companyName) customer.setValue({ fieldId: 'companyname', value: data.companyName });
+        if (data.email) customer.setValue({ fieldId: 'email', value: data.email });
+        if (data.phone) customer.setValue({ fieldId: 'phone', value: data.phone });
+        if (data.address) customer.setValue({ fieldId: 'defaultaddress', value: data.address });
+        
+        var updatedId = customer.save();
+        log.audit({
+            title: 'Customer Updated',
+            details: 'Customer updated with ID: ' + updatedId
+        });
+        return {
+            success: true,
+            message: 'Customer updated successfully',
+            id: updatedId
+        };
+    }
+
+    function handleCustomerDelete(customerId) {
+        record.delete({
+            type: record.Type.CUSTOMER,
+            id: customerId
+        });
+        log.audit({
+            title: 'Customer Deleted',
+            details: 'Customer deleted with ID: ' + customerId
+        });
+        return {
+            success: true,
+            message: 'Customer deleted successfully',
+            id: customerId
+        };
+    }
+
+    // Helper functions for Item operations
+    function handleItemGet(itemId) {
+        if (itemId) {
+            var item = record.load({
+                type: record.Type.INVENTORY_ITEM,
+                id: itemId,
+                isDynamic: false
+            });
+            return {
+                success: true,
+                data: {
+                    id: item.id,
+                    itemId: item.getValue({ fieldId: 'itemid' }),
+                    displayName: item.getValue({ fieldId: 'displayname' }),
+                    description: item.getValue({ fieldId: 'salesdescription' }),
+                    cost: item.getValue({ fieldId: 'cost' })
+                }
+            };
+        } else {
+            var itemSearch = search.create({
+                type: search.Type.ITEM,
+                columns: ['internalid', 'itemid', 'displayname', 'salesdescription', 'cost'],
+                filters: []
+            });
+            var searchResults = itemSearch.run().getRange({ start: 0, end: 100 });
+            var items = searchResults.map(function(result) {
+                return {
+                    id: result.getValue('internalid'),
+                    itemId: result.getValue('itemid'),
+                    displayName: result.getValue('displayname'),
+                    description: result.getValue('salesdescription'),
+                    cost: result.getValue('cost')
+                };
+            });
+            return {
+                success: true,
+                data: items
+            };
+        }
+    }
+
+    function handleItemPost(data) {
+        var item = record.create({
+            type: record.Type.INVENTORY_ITEM,
+            isDynamic: true
+        });
+        item.setValue({ fieldId: 'itemid', value: data.itemId });
+        if (data.displayName) item.setValue({ fieldId: 'displayname', value: data.displayName });
+        if (data.description) item.setValue({ fieldId: 'salesdescription', value: data.description });
+        if (data.cost) item.setValue({ fieldId: 'cost', value: data.cost });
+        
+        var itemId = item.save();
+        log.audit({
+            title: 'Item Created',
+            details: 'Item created with ID: ' + itemId
+        });
+        return {
+            success: true,
+            message: 'Item created successfully',
+            id: itemId
+        };
+    }
+
+    function handleItemPut(itemId, data) {
+        var item = record.load({
+            type: record.Type.INVENTORY_ITEM,
+            id: itemId,
+            isDynamic: true
+        });
+        if (data.itemId) item.setValue({ fieldId: 'itemid', value: data.itemId });
+        if (data.displayName) item.setValue({ fieldId: 'displayname', value: data.displayName });
+        if (data.description) item.setValue({ fieldId: 'salesdescription', value: data.description });
+        if (data.cost) item.setValue({ fieldId: 'cost', value: data.cost });
+        
+        var updatedId = item.save();
+        log.audit({
+            title: 'Item Updated',
+            details: 'Item updated with ID: ' + updatedId
+        });
+        return {
+            success: true,
+            message: 'Item updated successfully',
+            id: updatedId
+        };
+    }
+
+    function handleItemDelete(itemId) {
+        record.delete({
+            type: record.Type.INVENTORY_ITEM,
+            id: itemId
+        });
+        log.audit({
+            title: 'Item Deleted',
+            details: 'Item deleted with ID: ' + itemId
+        });
+        return {
+            success: true,
+            message: 'Item deleted successfully',
+            id: itemId
+        };
+    }
+
+    return {
+        'get': doGet,
+        'put': doPut,
+        'post': doPost,
+        'delete': doDelete
+    };
+});
